65079a693fc4798713d5ab9656df6108b800a952
felipedofd <felipedofd@hotmail.com>
2021-06-26 16:53:30 -0300

    criando função origem

--- pacote.py
+++ pacote.py
@@ -3,3 +3,15 @@ class Pacote:
         self.codigo = codigo
 
     def funcao_origem(self):
+        if self.codigo[0:3] == "111":
+            return "Centro-oeste"
+        elif self.codigo[0:3] == "333":
+            return "Nordeste"
+        elif self.codigo[0:3] == "555":
+            return "Norte"
+        elif self.codigo[0:3] == "888":
+            return "Sudeste"
+        elif self.codigo[0:3] == "000":
+            return "Sul"
+        else:
+            return "INVÁLIDO"
