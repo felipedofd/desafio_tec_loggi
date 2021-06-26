27afff8e2a0fb7b5c9bbdf6825a3b866e71cb277
felipedofd <felipedofd@hotmail.com>
2021-06-26 16:53:47 -0300

    criando função destino

--- pacote.py
+++ pacote.py
@@ -15,3 +15,17 @@ class Pacote:
             return "Sul"
         else:
             return "INVÁLIDO"
+
+    def funcao_destino(self):
+        if self.codigo[3:6] == "111":
+            return "Centro-oeste"
+        elif self.codigo[3:6] == "333":
+            return "Nordeste"
+        elif self.codigo[3:6] == "555":
+            return "Norte"
+        elif self.codigo[3:6] == "888":
+            return "Sudeste"
+        elif self.codigo[3:6] == "000":
+            return "Sul"
+        else:
+            return "INVÁLIDO"
